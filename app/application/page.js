c2bd61c3372e1327db753d5420508487f2c68ca7
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-02-15 00:19:31 +0800

    application page

---
+++ app/application/page.js
@@ -0,0 +1,49 @@
+import Link from "next/link"
+
+export default function Application() {
+    return (
+        <div className="flex justify-center w-full flex-col font-serif">
+            <span className="font-bold text-lg text-center">2024暑期班入學申請程序</span>
+            <span className="font-bold text-lg text-center">Application Procedure for Summer Classes 2024</span>
+            <span className="text-base text-center">報名日期：<span className="text-red-500">2024年4月8日 - 6月3日晚上11時59分</span></span>
+            <span className="text-base text-center"> Application period: <span className="text-red-500">8 April - 3 June 2024 (11:59pm)</span></span>
+            <br />
+            <span className="font-bold text-base">第一步：申請表</span>
+            <span className="font-bold text-base">STEP 1 : Online Application Form</span>
+            <span className="text-sm">請依照下列步驟完成網上申請。</span>
+            <span className="text-sm">Please follow the steps to complete online application.</span>
+            <span className="text-sm mt-1">1. 登入<Link href="https://cloud.itsc.cuhk.edu.hk/SAYT/Login.aspx" className="text-[#993300]">SAYT申請系統</Link>。(首次登入須先以電郵登記帳戶，EPYMT舊生亦須透過有效帳戶登入。)</span>
+            <span className="text-sm mb-1">1. Log into <Link href="https://cloud.itsc.cuhk.edu.hk/SAYT/Login.aspx" className="text-[#993300]">SAYT Application System</Link>. (For first-time user, please register an account with your email. Even you were former EPYMT student, you still need a valid account to login.)</span>
+            <span className="text-sm mt-1">2. 根據指示填寫申請表。</span>
+            <span className="text-sm mb-1">2. Complete the application form.</span>
+            <span className="text-sm">請留意以下事項:</span>
+            <span className="text-sm">Please be reminded:</span>
+            <span className="text-sm">- 曾報讀EPYMT課程之舊生, 必須填寫曾報讀之科目。</span>
+            <span className="text-sm">- Former EPYMT students must complete the form at the link under &quot;Former EPYMT Applicant&quot; of the online application.</span>
+            <span className="text-sm">- 申請EPYMT課程每科須繳交港幣50元報名費。</span>
+            <span className="text-sm">- Application fee is HK$50 for each EPYMT course.</span>
+            <span className="text-sm">- 申請人如申請多於一個課程，請於同一帳戶內遞交多份申請表。</span>
+            <span className="text-sm">- Applicants who apply for more than one course should submit application separately for each course in the same account.</span>
+            <span className="text-sm mt-1">3. 登入帳戶內確認申請狀況，日後可查看成績。</span>
+            <span className="text-sm mb-1">3. Check application status. Applicants can also login the account to check their course result in the future.</span>
+            <span className="text-xs text-red-500">* 現階段不用另外寄交文件或證書。</span>
+            <span className="text-xs text-red-500">* It is not necessary to send any supporting documents at this stage.</span>
+            <br />
+            <span className="font-bold text-base">第二步： 入學試</span>
+            <span className="font-bold text-base">STEP 2 : Admission Screening Test</span>
+            <span className="text-sm font-bold">舊生</span> 
+            <span className="text-sm font-bold">Former students</span> 
+            <span className="text-sm mt-1">舊生或須出席入學試。請細閱所報科目的網頁列出的免試條款，並於網上申請表「舊生」一欄的連結上，準確填寫所需資料。</span> 
+            <span className="text-sm mb-1">Former students of EPYMT may enjoy exemption of admission screening test under certain conditions. Please read the course webpage for the requirements, and complete the form at the link under &quot;Former EPYMT Applicant&quot; of the online application.</span> 
+            <span className="text-sm font-bold">本地學生 和 海外學生</span> 
+            <span className="text-sm font-bold">Local and Overseas students</span> 
+            <span className="text-sm mt-1">所有本地首次申請人均須參加於 <span className="text-red-500">2024年6月8日（星期六）</span>舉行的入學試。缺席者作放棄論。</span> 
+            <span className="text-sm mb-1">All local first-time applicants must attend the admission screening test on <span className="text-red-500">8 June 2024 (Sat)</span>. Failing to do so will be considered as withdrawing from application.</span> 
+            <span className="text-sm mt-1 text-green-500">入學試詳情請參閱<Link href="screening" className="text-[#993300]">這裡</Link></span> 
+            <span className="text-sm mb-1 text-green-500">For test details, please click <Link href="screening" className="text-[#993300]">here</Link></span>
+            <br />
+            <span className="text-xs">注意：本課程<span className="text-bold text-green-500">編排緊密</span>，學生須妥善安排時間，<span className="text-bold text-green-500">積極參與</span>，方能獲得最大學習成效。學生退學或無故缺課，將按照大學規章嚴格處理。</span>
+            <span className="text-xs">Attention: The learning pace is fast and the schedule is tight. It <span className="text-bold text-green-500">requires tremendous effort</span> and commitment of the student to truly benefit from our programme. Absence and withdrawal are detrimental to learning, and we will also strictly follow the university regulations on matters related to taking leave and withdrawal.</span>
+        </div>
+    )
+};
---
+++ app/screening/page.js
@@ -0,0 +1,51 @@
+import Link from "next/link";
+
+export default function Screening() {
+    return (
+        <div className="flex justify-center w-full flex-col text-base font-serif">
+            <span className="font-bold text-lg text-center font-serif">Announcement of Admission Screening Test</span>
+            <br />
+            <span>If you are not a former EPYMT student, you MUST attend the take-home screening test.</span>
+            <br />
+            <span className="font-bold">Information of the take-home test:</span>
+            <table className="table-auto">
+                <tbody>
+                    <tr>
+                        <td>Test Date and Time: </td>
+                        <td><span className="text-red-500">10:30 a.m. on 8 June 2024 (Saturday)</span></td>
+                    </tr>
+                    <tr>
+                        <td>Submission Time: </td>
+                        <td><span className="text-red-500">00:00 to 23:59 on 11 June 2024 (Tuesday)</span></td>
+                    </tr>
+                    <tr>
+                        <td>Language: </td>
+                        <td>English</td>
+                    </tr>
+                    <tr>
+                        <td>Number of test paper: </td>
+                        <td>1 only</td>
+                    </tr>
+                    <tr>
+                        <td>Content: </td>
+                        <td>From anywhere in mathematics; including something beyond secondary school curriculum</td>
+                    </tr>
+                </tbody>
+            </table>
+            <br />
+            <span className="font-bold">Points to Note:</span>
+            <span>1. Please ensure that your email accounts have enough mail space.</span>
+            <span>2. Please add our email address (<Link href="mailto:epymt@math.cuhk.edu.hk" className="text-[#993300]">epymt@math.cuhk.edu.hk</Link>) into your address book so that our message will not be moved to junk mails.</span>
+            <span>3. If you have updated email address, please inform us as soon as possible.</span>
+            <span>4. Please check your email frequently these days.</span>
+            <span>5. If you are overseas applicants with different time zones, please let us know by email in advance for special arrangement.</span>
+            <br />
+            <span className="font-bold">**Important**</span>
+            <span>1. The test paper will be sent to you by email on 8 June (Saturday, 10:30 a.m. Hong Kong Time).</span>
+            <span>2. Please return your answers from 00:00 to 23:59 on 11 June (next Tuesday) by email to <Link href="mailto:epymt@math.cuhk.edu.hk" className="text-[#993300]">epymt@math.cuhk.edu.hk</Link>.</span>
+            <br />
+            <span className="text-sm">Enquiries: <Link href="mailto:epymt@math.cuhk.edu.hk" className="text-[#993300]">epymt@math.cuhk.edu.hk</Link></span>
+
+        </div>
+    );
+}
