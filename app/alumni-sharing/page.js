18b0e4be0c22812cda60e001a21d48a788b36205
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-02-15 00:36:29 +0800

    Update next.config.mjs for production settings and asset prefix
    Update layout and page components to use new basePath for image sources
    
    Fix typo in summer classes application link
    
    Add new pages for Alumni Sharing, Contact Us, Courses, FAQ, and Guest Lectures

---
+++ app/alumni-sharing/page.js
@@ -0,0 +1,5 @@
+export default function AlumniSharing() {
+    return (
+        <div></div>
+    );
+};
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+    output: "export",
     basePath: "/epymt",
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
