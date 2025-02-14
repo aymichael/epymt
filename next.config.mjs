d577682427123afef36ff9a56b915170fa3219ea
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-02-15 00:36:29 +0800

    Update next.config.mjs for production settings and asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
-    basePath: "/epymt",
-};
+    output: isProd ? 'export' : undefined,
+    reactStrictMode: true,
+    assetPrefix: isProd
+      ? 'https://ngyukfung.github.io/epymt/'
+      : undefined
+  }
 
 export default nextConfig;
