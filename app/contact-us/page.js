d9d2465f58bdd7da89d3215d858c2c121c3753ea
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-02-15 11:01:21 +0800

    contact page

--- app/contact-us/page.js
+++ app/contact-us/page.js
@@ -1,5 +1,37 @@
+import Link from "next/link";
+
 export default function Contact() {
     return (
-        <div></div>
+        <div className="flex justify-center w-full flex-col gap-2 font-serif sm:text-base text-sm">
+            <span className="font-bold text-lg">查詢及聯絡方法 Contact</span>
+            <table className="table-auto">
+                <tbody>
+                    <tr>
+                        <td className="pr-4 py-1 font-bold align-top">地址 Address</td>
+                        <td className="pl-4 py-1">
+                            <div className="flex flex-col gap-1">
+                                <span>新界沙田香港中文大學數學系</span>
+                                <span>數學英才精進課程</span>
+                                <span>EPYMT, Department of Mathematics</span>
+                                <span>The Chinese University of Hong Kong, Shatin, N.T., Hong Kong</span>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="pr-4 py-1 font-bold">電話 Phones</td>
+                        <td className="pl-4 py-1">(852) 3943 5295</td>
+                    </tr>
+                    <tr>
+                        <td className="pr-4 py-1 font-bold">傳真 Fax</td>
+                        <td className="pl-4 py-1">(852) 2603 5154</td>
+                    </tr>
+                    <tr>
+                        <td className="pr-4 py-1 font-bold">電郵 Email</td>
+                        <td className="pl-4 py-1"><Link href="mailto:epymt@math.cuhk.edu.hk" className="text-[#993300]">epymt@math.cuhk.edu.hk</Link></td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
     );
 };
