4ba01d94e2486eab0575d44d80d5b35406b323fa
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-03-19 23:32:11 +0800

    refactor contact us page layout and improve navbar styles for better darkmode

--- app/navbar.js
+++ app/navbar.js
@@ -17,8 +17,8 @@ export default function Navbar() {
 
     return (
         <header className="flex select-none w-full flex-col text-lg/8 md:shadow-md fixed md:items-center" >
-            <div className={` dark:bg-gray-700 md:shadow-none ${isOpen ? "" : "shadow-md"} flex flex-col w-full md:px-0 px-2 items-center`} onMouseLeave={() => setCurrentNav(-1)}>
-                <div className="bg-white md:w-[48rem] lg:w-[64rem] flex flex-row items-center w-full justify-between h-16">
+            <div className={` md:shadow-none ${isOpen ? "" : "shadow-md"} flex flex-col w-full md:px-0 px-2 items-center`} onMouseLeave={() => setCurrentNav(-1)}>
+                <div className="bg-white dark:bg-gray-700 md:w-[48rem] lg:w-[64rem] flex flex-row items-center w-full justify-between h-16">
                     <div>
                         <Link href="/">
                             <Image
@@ -59,7 +59,7 @@ export default function Navbar() {
                         {pages.map((page, index) => (
                             <div
                                 key={index}
-                                className={`h-full flex items-center px-2 ${currentNav === index ? "bg-blue-100" : ""}`}
+                                className={`h-full flex items-center px-2 ${currentNav === index ? "bg-blue-100 dark:bg-sky-800" : ""}`}
                             >
                                 <span className="hover:translate-y-[-3px] transition-all duration-200 ease-in-out"
                                     onMouseEnter={() => setCurrentNav(index)}>{page}</span>
@@ -67,7 +67,7 @@ export default function Navbar() {
                         ))}
                     </nav>
                 </div>
-                {currentNav >= 0 ? <div className={`bg-blue-100 w-full shadow-md h-16 flex justify-center`}>
+                {currentNav >= 0 ? <div className={`dark:bg-sky-800 bg-blue-100 w-full shadow-md h-16 md:flex hidden justify-center`}>
                     <nav className="flex flex-row md:w-[48rem] lg:w-[64rem] items-center py-2 justify-end gap-2">
                         {subpages[currentNav].map((subpage, index) => (
                             <Link
@@ -82,19 +82,19 @@ export default function Navbar() {
                 </div> : null}
             </div>
             <div className={`w-full md:hidden h-screen ${isOpen ? "bg-black/40" : ""}`}>
-                <nav className={`flex bg-white/95 flex-col items-start transition-all duration-500 ease-in-out ${isOpen ? "shadow-md scale-y-100" : "scale-y-0"} pt-2 origin-top`}>
+                <nav className={`flex bg-white/95 dark:bg-gray-700/95 flex-col items-start transition-all duration-500 ease-in-out ${isOpen ? "shadow-md scale-y-100" : "scale-y-0"} pt-2 origin-top`}>
                     {isOpen && pages.map((page, index) => (
                         <div
                             key={index}
                             className="w-full flex flex-col last:border-none border-b border-gray-300 cursor-pointer"
-                            
+
                         >
                             <div className="flex mx-4 my-2 flex-row justify-between items-center" onClick={() => setSelectedNav(index === selectedNav ? -1 : index)}>
                                 {page}
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" className={`transition-all duration-300 ease-in-out ${selectedNav === index ? "rotate-180" : ""}`}
+                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" className={`transition-all duration-300 ease-in-out  dark:fill-white ${selectedNav === index ? "rotate-180" : ""}`}
                                 ><path d="M0 7.33l2.829-2.83 9.175 9.339 9.167-9.339 2.829 2.83-11.996 12.17z" /></svg>
                             </div>
-                            <div className={`flex flex-col transition-all ease-in-out duration-300 origin-top gap-2 bg-blue-100`}>
+                            <div className={`flex flex-col transition-all ease-in-out duration-300 origin-top gap-2 bg-blue-100 dark:bg-sky-800`}>
                                 {selectedNav === index ? subpages[index].map((subpage, subindex) => (
                                     <Link
                                         key={subindex}
@@ -103,15 +103,15 @@ export default function Navbar() {
                                         onClick={() => setIsOpen(false)}
                                     >
                                         {subpage}
-                                    </Link> 
-                                )): null}
+                                    </Link>
+                                )) : null}
                             </div>
                         </div>
 
                     ))}
                 </nav>
                 <div className="w-full h-full" onClick={() => setIsOpen(false)}></div>
-                </div>
+            </div>
             <div className={`w-full h-screen ${currentNav === -1 ? "hidden" : ""} bg-black/40`}></div>
         </header>
     );
