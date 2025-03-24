9fbccdcd685126f1554a7474819de1c783a310d3
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-03-24 18:17:59 +0800

    resize font size and add i18n for advisors page

--- app/[lng]/advisors/page.js
+++ app/[lng]/advisors/page.js
@@ -1,101 +1,100 @@
+import { translation } from "@/app/i18n-index";
 
-
-export default function Advisors() {
+export default async function Advisors({ params }) {
+    const { lng } = await params;
+    const { t } = await translation(lng, "advisors");
     return (
         <div className="flex justify-center w-full flex-col gap-2">
-            <span className="font-bold text-lg/8">初始學術顧問委員 Inaugurate Advisors</span>
-            <span className="text-sm/8">
-                本課程的初始顧問來自世界各地著名大學，大部份曾任系主任、院長、甚至校長。 他們一方面位處世界學問前端，另一方面也了解大學教育和收生等行政事務。他們也致力於培養各地優秀的年青人，全賴他們提供之寶貴意見，為本課程的發展奠下基礎。
-            </span>
-            <span className="text-sm/8">
-                The inaugurate advisors for our programme are coming from the most famous universities in the world. They had been department heads, faculty deans, or even presidents in their universities. On the one hand, they are at the frontier of academic research; on the other hand, they are well acquainted with university education and administration such as admission. They are also committed to cultivating outstanding young people from all over the world, and their valuable contributions laid the foundation for the development of our programme.
+            <span className="font-bold text-xl/8">{t("title")}</span>
+            <span>
+                {t("introduction")}
             </span>
             <div className="flex justify-center">
-                <table className="w-3/4 table-auto border border-collapse border-black text-sm/8">
-                    <thead>
-                        <tr className="border border-black bg-[#bdab79]">
-                            <th className="text-left px-2 py-1">NAME</th>
-                            <th className="text-left px-2 py-1">ORGANIZATION</th>
+                <table className="w-full table-auto ">
+                    <thead class=" text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th className="text-left px-2 py-1">{t("table.name")}</th>
+                            <th className="text-left px-2 py-1">{t("table.organization")}</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Philip Candelas</td>
                             <td className="px-2 py-1">Oxford University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Tony Chan 陳繁昌教授</td>
                             <td className="px-2 py-1">University of California, Los Angeles</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Man-Duen Choi 蔡文端教授</td>
                             <td className="px-2 py-1">University of Toronto</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Cho-Ho Chu 朱礎豪教授</td>
                             <td className="px-2 py-1">London University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. John Chu 朱嘉鯤教授</td>
                             <td className="px-2 py-1">Columbia University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. J. H. Coates*</td>
                             <td className="px-2 py-1">Cambridge University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Benedict Gross</td>
                             <td className="px-2 py-1">Harvard University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Chaohao Gu 谷超豪教授</td>
                             <td className="px-2 py-1">Wenzhou University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Jean Pierre Kahane</td>
                             <td className="px-2 py-1">Paris University, Orsay</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Hsiang-Tsung Kung</td>
                             <td className="px-2 py-1">Harvard University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Tsz-Leung Lai 黎子良教授</td>
                             <td className="px-2 py-1">Stanford University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Rafe R. Mazzeo</td>
                             <td className="px-2 py-1">Stanford University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Duong H. Phong</td>
                             <td className="px-2 py-1">Columbia University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Wilfried Schmid</td>
                             <td className="px-2 py-1">Harvard University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. R. Sommer</td>
                             <td className="px-2 py-1">Stanford University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Daniel Stroock</td>
                             <td className="px-2 py-1">M.I.T.</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Neil S. Trudinger</td>
                             <td className="px-2 py-1">Australian National University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Wing-Hung Wong 王永雄教授</td>
                             <td className="px-2 py-1">Harvard University</td>
                         </tr>
-                        <tr className="border border-black bg-[#f0e0c0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Andrew C. C. Yao</td>
                             <td className="px-2 py-1">Princeton University</td>
                         </tr>
-                        <tr className="border border-black bg-[#e0d0b0]">
+                        <tr class="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                             <td className="px-2 py-1">Prof. Gregg J. Zuckerman</td>
                             <td className="px-2 py-1">Yale University</td>
                         </tr>
@@ -103,7 +102,7 @@ export default function Advisors() {
                 </table>
             </div>
             <span className="text-xs">
-                * We are deeply saddened to learn that Prof. J. H. Coates, passed away in May 2022. He will always be remembered for his great contributions to our programme.
+                {t("remark")}
             </span>
         </div>
     );
