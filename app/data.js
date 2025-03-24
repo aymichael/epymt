1376d052af2224874d213d0350f40a3c324c1702
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-03-24 20:11:15 +0800

    add data file for future edit

---
+++ app/data.js
@@ -0,0 +1,12 @@
+
+
+// Year of admission
+export const year = 2024;
+
+// Application period
+export const applicationPeriodCHI = "2024年4月8日 - 6月3日晚上11時59分";
+export const applicationPeriodENG = "8 April - 3 June 2024 (11:59pm)";
+
+// Admission test date
+export const admissionTestDateCHI = "2024年6月8日（星期六）";
+export const admissionTestDateENG = "8 June 2024 (Sat)";
