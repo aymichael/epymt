c6b8ca4925ed6655f7b70677b2510a792246787a
Ng Yuk Fung <53886955+ngyukfung@users.noreply.github.com>
2025-06-24 15:14:26 +0800

    support for viewing old website

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ acceptLanguage.languages(languages)
 
 export const config = {
   // matcher: '/:lng*'
-  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|[^./]+.(?:jpg|png|gif)|leaflet&poster).*)']
+  matcher: ['/((?!api|_next/static|_next/image|assets|favicon.ico|sw.js|[^./]+.(?:jpg|png|gif)|leaflet&poster|old).*)']
 }
 
 export function middleware(req) {
